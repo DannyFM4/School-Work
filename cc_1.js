f0e16bcac137380cccba8e4d219cd18f4694e739
Daniel Feliciano-Munoz <danielfelicianomunoz@usf.edu>
2025-01-24 16:48:26 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -6,3 +6,12 @@ var isActive = true;
 console.log("Name: ", employeeName, "Type: ", typeof employeeName);
 console.log("Number: ", employeeID, "Type: ", typeof employeeID);
 console.log("Value: ", isActive, "Type: ", typeof isActive);
+
+//Task 2: Primitive Data Types
+let productName = "T-Shirt";
+const productPrice = 20;
+var isAvailable = true;
+
+console.log("Item: ", productName, "Type: ", typeof productName);
+console.log("Cost: $", productPrice, "Type: ", typeof productPrice);
+console.log("In Stock: ", isAvailable, "Type: ", typeof isAvailable);
