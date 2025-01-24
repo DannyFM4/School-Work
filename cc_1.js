ffb214a653aa34840db64661b920c5db66966193
Daniel Feliciano-Munoz <danielfelicianomunoz@usf.edu>
2025-01-24 16:34:29 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,8 @@
+//Task 1: Variables
+let employeeName = "John Doe";
+const employeeID = 101;
+var isActive = true;
+
+console.log("Name: ", employeeName, "Type: ", typeof employeeName);
+console.log("Number: ", employeeID, "Type: ", typeof employeeID);
+console.log("Value: ", isActive, "Type: ", typeof isActive);
